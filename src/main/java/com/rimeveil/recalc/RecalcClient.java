2ad386c2abc23dcad3053b84c2bf0063fe83c3b4
Rimeveil <bpg662266@gmail.com>
2026-03-01 23:04:49 +0800

    Fabric Mod

---
+++ src/main/java/com/rimeveil/recalc/RecalcClient.java
@@ -0,0 +1,10 @@
+package com.rimeveil.recalc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RecalcClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/rimeveil/recalc/RecalcDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rimeveil.recalc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RecalcDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
