8cba676bd0e2ae3986de066cd9d42ba776598975
Rimeveil <bpg662266@gmail.com>
2026-03-02 00:48:34 +0800

    feat(物品): 将示例物品添加到自然物品组

--- src/main/java/com/rimeveil/recalc/Item/Moditem.java
+++ src/main/java/com/rimeveil/recalc/Item/Moditem.java
@@ -1,8 +1,10 @@
 package com.rimeveil.recalc.Item;
 
 import com.rimeveil.recalc.Recalc;
-
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -26,7 +28,16 @@ public class Moditem {
     public static Item regitem(String id, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Recalc.MOD_ID, id), item);
     }
-    public static void regitems() {
 
+    /**
+     * 将物品添加到自然物品组
+     * @param entries 物品组条目
+     */
+    public static void regitemtogroup(FabricItemGroupEntries entries) {
+        entries.add(EXAMPLE_ITEM);
+    }
+
+    public static void regitems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(Moditem::regitemtogroup);
     }
 }
