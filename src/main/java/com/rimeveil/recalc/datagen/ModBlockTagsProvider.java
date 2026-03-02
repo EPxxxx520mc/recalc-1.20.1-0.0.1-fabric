54e172effe8ba42a69585fe3529165433a54dc6c
Rimeveil <bpg662266@gmail.com>
2026-03-02 15:50:58 +0800

    gg

--- src/main/java/com/rimeveil/recalc/datagen/ModBlockTagsProvider.java
+++ src/main/java/com/rimeveil/recalc/datagen/ModBlockTagsProvider.java
@@ -9,5 +9,21 @@ import net.minecraft.registry.tag.BlockTags;
 import java.util.concurrent.CompletableFuture;
 
 public class ModBlockTagsProvider extends FabricTagProvider.BlockTagProvider{
-    
+    public ModBlockTagsProvider(FabricDataOutput Output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(Output, registriesFuture);
+    }
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+            .add(Modblock.EXAMPLE_BLOCK)
+            .add(Modblock.EXAMPLE_BLOCK2)
+            .add(Modblock.EXAMPLE_BLOCK3)
+            .add(Modblock.EXAMPLE_BLOCK4);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_STONE_TOOL)
+            .add(Modblock.EXAMPLE_BLOCK)
+            .add(Modblock.EXAMPLE_BLOCK2)
+            .add(Modblock.EXAMPLE_BLOCK3)
+            .add(Modblock.EXAMPLE_BLOCK4);
+    }
 }
