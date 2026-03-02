8190539e66640db50f352ee2128fba672749e345
Rimeveil <bpg662266@gmail.com>
2026-03-02 15:49:32 +0800

    new

---
+++ src/main/java/com/rimeveil/recalc/datagen/ModBlockTagsProvider.java
@@ -0,0 +1,13 @@
+package com.rimeveil.recalc.datagen;
+
+import com.rimeveil.recalc.block.Modblock;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagsProvider extends FabricTagProvider.BlockTagProvider{
+    
+}
