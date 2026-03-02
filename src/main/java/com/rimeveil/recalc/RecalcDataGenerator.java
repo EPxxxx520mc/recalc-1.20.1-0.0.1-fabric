91d3652eff011c876a3ea5354593ccedab1a0cb2
Rimeveil <bpg662266@gmail.com>
2026-03-02 15:53:38 +0800

    ok

--- src/main/java/com/rimeveil/recalc/RecalcDataGenerator.java
+++ src/main/java/com/rimeveil/recalc/RecalcDataGenerator.java
@@ -1,11 +1,13 @@
 package com.rimeveil.recalc;
 
+import com.rimeveil.recalc.datagen.ModBlockTagsProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class RecalcDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(ModBlockTagsProvider::new);
 	}
 }
