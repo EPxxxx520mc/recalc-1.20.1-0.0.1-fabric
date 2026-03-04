96c20d66ee2d655a1397e3ce9205e5ed2c9e5deb
Rimeveil <bpg662266@gmail.com>
2026-03-04 15:55:47 +0800

    g

--- src/main/java/com/rimeveil/recalc/Item/ModFoodComponents.java
+++ src/main/java/com/rimeveil/recalc/Item/ModFoodComponents.java
@@ -5,14 +5,15 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent CORN = new FoodComponent.Builder().hunger(2).saturationModifier(0.3F).build();
+    public static final FoodComponent CORN = new FoodComponent.Builder().hunger(4).saturationModifier(0.5F).build();
     public static final FoodComponent SUPER_SUGAR = new FoodComponent.Builder().hunger(10).saturationModifier(1F)
+        .meat()
         .alwaysEdible()
         .statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 200), 1.0F)
         .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 200), 1.0F)
         .statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 200), 2.0F)
         .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 200), 2.0F)
-        .statusEffect(new StatusEffectInstance(StatusEffects.LUCK, 200), 10.0F)
+        .statusEffect(new StatusEffectInstance(StatusEffects.LUCK, 2400), 10.0F)
         .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200), 5.0F)
         .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 200), 3.0F)
         .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 200), 1.0F)
