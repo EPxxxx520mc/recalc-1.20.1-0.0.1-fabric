ea7554c6ada0818bb490be5613b1d30febb0d1d4
Rimeveil <bpg662266@gmail.com>
2026-03-07 16:37:45 +0800

    1

---
+++ src/main/java/com/rimeveil/recalc/Item/custom/ModarmorItem.java
@@ -0,0 +1,62 @@
+package com.rimeveil.recalc.Item.custom;
+
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import net.fabricmc.loader.impl.discovery.DomainObject.Mod;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import com.rimeveil.recalc.Item.ModArmorMaterials;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+public class ModarmorItem extends ArmorItem{
+    private static final Map<ArmorMaterial, List<StatusEffectInstance>> MAP = 
+        (new ImmutableMap.Builder<ArmorMaterial, List<StatusEffectInstance>>()
+            .put(ModArmorMaterials.SUPER_SUGAR,
+                Arrays.asList(
+                    new StatusEffectInstance(StatusEffects.SPEED, 20 * 60, 1,false,false,true),
+                    new StatusEffectInstance(StatusEffects.JUMP_BOOST, 20 * 60, 1,false,false,true)
+            ))
+            .build());
+    public ModarmorItem(ArmorMaterial material, Type type, Settings settings) {
+        super(material, type, settings);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (!world.isClient) {
+            if (entity instanceof PlayerEntity player && hasFullSuitableArmor(player)) {
+                    evaluateArmorEffect(player);
+            }
+        
+        }
+            super.inventoryTick(stack, world, entity, slot, selected);
+    }
+
+    private void evaluateArmorEffect(PlayerEntity player) {
+        for (Map.Entry<ArmorMaterial, List<StatusEffectInstance>> entry : MAP.entrySet()) {
+            ArmorMaterial material = entry.getKey();
+            List<StatusEffectInstance> effect = entry.getValue();1    
+        
+            if (hasCorrectMaterialArmorOn(material,player)) {
+                for (StatusEffectInstance instance : effect) {
+                    StatusEffect effect1 = effect.getEffectType();
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean hasFullSuitableArmor(PlayerEntity player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasFullSuitableArmor'");
+    }
+}
