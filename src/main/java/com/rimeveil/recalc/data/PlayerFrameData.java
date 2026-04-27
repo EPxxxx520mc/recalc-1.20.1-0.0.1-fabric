f5e136e211eb55a7fd5b56ace50b48546f895b8a
Rimeveil <bpg662266@gmail.com>
2026-04-27 20:35:07 +0800

    76

--- src/main/java/com/rimeveil/recalc/data/PlayerFrameData.java
+++ src/main/java/com/rimeveil/recalc/data/PlayerFrameData.java
@@ -1,8 +1,11 @@
 package com.rimeveil.recalc.data;
 
+import com.rimeveil.recalc.Recalc;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.PersistentState;
+import net.minecraft.world.PersistentStateManager;
 import net.minecraft.world.World;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,39 +13,75 @@ import java.util.UUID;
 
 public class PlayerFrameData {
     private static final Map<UUID, Boolean> clientCache = new HashMap<>();
+    private static FrameState state;
+
+    private static class FrameState extends PersistentState {
+        private final Map<UUID, Boolean> players = new HashMap<>();
+
+        public FrameState() {
+        }
+
+        public static FrameState fromNbt(NbtCompound nbt) {
+            FrameState state = new FrameState();
+            NbtCompound playersNbt = nbt.getCompound("players");
+            for (String key : playersNbt.getKeys()) {
+                UUID uuid = UUID.fromString(key);
+                state.players.put(uuid, playersNbt.getBoolean(key));
+            }
+            Recalc.LOGGER.info("Loaded frame data: " + state.players.size() + " players");
+            return state;
+        }
+
+        @Override
+        public NbtCompound writeNbt(NbtCompound nbt) {
+            NbtCompound playersNbt = new NbtCompound();
+            for (Map.Entry<UUID, Boolean> entry : players.entrySet()) {
+                playersNbt.putBoolean(entry.getKey().toString(), entry.getValue());
+            }
+            nbt.put("players", playersNbt);
+            Recalc.LOGGER.info("Saved frame data: " + players.size() + " players");
+            return nbt;
+        }
+    }
+
+    private static FrameState getState(World world) {
+        if (world.isClient) {
+            return null;
+        }
+        ServerWorld serverWorld = (ServerWorld) world;
+        // Get overworld
+        ServerWorld overworld = serverWorld.getServer().getOverworld();
+        PersistentStateManager manager = overworld.getPersistentStateManager();
+        return manager.getOrCreate(FrameState::fromNbt, FrameState::new, "recalc_frame_data");
+    }
 
     public static boolean hasFrameAttached(PlayerEntity player) {
         World world = player.getWorld();
-        if (world instanceof ServerWorld serverWorld) {
-            // Always use overworld to save
-            MinecraftServer server = serverWorld.getServer();
-            ServerWorld overworld = server.getOverworld();
-            PlayerFrameSavedData data = PlayerFrameSavedData.get(overworld);
-            return data.hasFrameAttached(player.getUuid());
+        if (world.isClient) {
+            return clientCache.getOrDefault(player.getUuid(), false);
         }
-        return clientCache.getOrDefault(player.getUuid(), false);
+        FrameState state = getState(world);
+        return state.players.getOrDefault(player.getUuid(), false);
     }
 
     public static void attachFrame(PlayerEntity player) {
         World world = player.getWorld();
-        if (world instanceof ServerWorld serverWorld) {
-            // Always use overworld to save
-            MinecraftServer server = serverWorld.getServer();
-            ServerWorld overworld = server.getOverworld();
-            PlayerFrameSavedData data = PlayerFrameSavedData.get(overworld);
-            data.attachFrame(player.getUuid());
+        if (!world.isClient) {
+            FrameState state = getState(world);
+            state.players.put(player.getUuid(), true);
+            state.markDirty();
+            Recalc.LOGGER.info("Attached frame to " + player.getName().getString());
         }
         clientCache.put(player.getUuid(), true);
     }
 
     public static void detachFrame(PlayerEntity player) {
         World world = player.getWorld();
-        if (world instanceof ServerWorld serverWorld) {
-            // Always use overworld to save
-            MinecraftServer server = serverWorld.getServer();
-            ServerWorld overworld = server.getOverworld();
-            PlayerFrameSavedData data = PlayerFrameSavedData.get(overworld);
-            data.detachFrame(player.getUuid());
+        if (!world.isClient) {
+            FrameState state = getState(world);
+            state.players.put(player.getUuid(), false);
+            state.markDirty();
+            Recalc.LOGGER.info("Detached frame from " + player.getName().getString());
         }
         clientCache.put(player.getUuid(), false);
     }
--- src/main/java/com/rimeveil/recalc/networking/ModNetworking.java
+++ src/main/java/com/rimeveil/recalc/networking/ModNetworking.java
@@ -2,34 +2,33 @@ package com.rimeveil.recalc.networking;
 
 import com.rimeveil.recalc.Recalc;
 import com.rimeveil.recalc.data.PlayerFrameData;
-import com.rimeveil.recalc.data.PlayerFrameSavedData;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 
 public class ModNetworking {
     public static final Identifier SYNC_FRAME_STATE = new Identifier(Recalc.MOD_ID, "sync_frame_state");
 
     public static void register() {
-        // Server-side
+        // Server side: when player joins
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
             ServerPlayerEntity player = handler.player;
-            ServerWorld overworld = server.getOverworld();
-            PlayerFrameSavedData data = PlayerFrameSavedData.get(overworld);
-            boolean hasFrame = data.hasFrameAttached(player.getUuid());
+            boolean hasFrame = PlayerFrameData.hasFrameAttached(player);
+            Recalc.LOGGER.info("Syncing frame state for " + player.getName().getString() + ": " + hasFrame);
+            
             PacketByteBuf buf = PacketByteBufs.create();
             buf.writeBoolean(hasFrame);
             ServerPlayNetworking.send(player, SYNC_FRAME_STATE, buf);
         });
 
-        // Client-side
+        // Client side: receive sync
         ClientPlayNetworking.registerGlobalReceiver(SYNC_FRAME_STATE, (client, handler, buf, responseSender) -> {
             boolean hasFrame = buf.readBoolean();
+            Recalc.LOGGER.info("Client received frame state: " + hasFrame);
             client.execute(() -> {
                 if (client.player != null) {
                     if (hasFrame) {
@@ -43,6 +42,7 @@ public class ModNetworking {
     }
 
     public static void syncToPlayer(ServerPlayerEntity player, boolean hasFrame) {
+        Recalc.LOGGER.info("Sending frame state to " + player.getName().getString() + ": " + hasFrame);
         PacketByteBuf buf = PacketByteBufs.create();
         buf.writeBoolean(hasFrame);
         ServerPlayNetworking.send(player, SYNC_FRAME_STATE, buf);
