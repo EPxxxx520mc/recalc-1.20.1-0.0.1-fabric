b788af8e9d069b281cdf3fdd75ea7107b2a06ea3
Rimeveil <bpg662266@gmail.com>
2026-04-28 18:11:49 +0800

    4

--- src/main/java/com/rimeveil/recalc/RecalcClient.java
+++ src/main/java/com/rimeveil/recalc/RecalcClient.java
@@ -2,12 +2,9 @@ package com.rimeveil.recalc;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
-import com.rimeveil.recalc.Item.Moditem;
 import com.rimeveil.recalc.block.Modblock;
 import com.rimeveil.recalc.client.ClientEventHandlers;
 import com.rimeveil.recalc.client.RecalcHudRenderer;
-import com.rimeveil.recalc.client.renderer.FictionalFrameRenderer;
 import com.rimeveil.recalc.keybind.ModKeybinds;
 
 public class RecalcClient implements ClientModInitializer {
@@ -17,9 +14,6 @@ public class RecalcClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(Modblock.EXAMPLE_BLOCK4, net.minecraft.client.render.RenderLayer.getTranslucent());
         BlockRenderLayerMap.INSTANCE.putBlock(Modblock.SUPER_SUGAR_TRAPDOOR, net.minecraft.client.render.RenderLayer.getTranslucent());
         BlockRenderLayerMap.INSTANCE.putBlock(Modblock.SUPER_SUGAR_DOOR, net.minecraft.client.render.RenderLayer.getTranslucent());
-        
-        // 注册 GeckoLib 物品渲染器
-        BuiltinItemRendererRegistry.INSTANCE.register(Moditem.FICTIONAL_FRAME, new FictionalFrameRenderer());
         
         ModKeybinds.register();
         ClientEventHandlers.register();
